1ba7e6cebe0abf3c0eccdb2abe8782d6eb01b989
Quinn94 <mayquinn94@gmail.com>
2020-04-05 18:37:41 +0800

    axios on screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,25 +4,39 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      this.setState({ posts: response.data });
+      // console.log(response)
+    });
+  }
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post title={post.title} key={post.id}/>;
+    });
+
+    return (
+      <div>
+        <section className="Posts">
+        {posts}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
