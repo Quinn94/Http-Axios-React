3b88c44cbe017ee058ee11d9ed54b8e7441f23f3
Quinn94 <mayquinn94@gmail.com>
2020-04-05 21:26:29 +0800

    Limit data, clickhandler

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,19 +8,36 @@ import axios from 'axios';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      this.setState({ posts: response.data });
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+        const posts = response.data.slice(0,4);
+        const updatedPosts = posts.map(post => {
+            return {
+                ...post, //distribute the property that we got from posts
+                author: 'Quinn' //then add a new property hard-code
+            }
+        });
+        this.setState({ posts: updatedPosts });
       // console.log(response)
     });
-  }
+  };
+
+  postSelectedHandler = (id) => {
+      this.setState({selectedPostId: id})
+  };
 
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post title={post.title} key={post.id}/>;
+      return <Post 
+      title={post.title} 
+      key={post.id} 
+      author={post.author}
+      clicked={()=>this.postSelectedHandler(post.id)}/>;
     });
 
     return (
@@ -29,7 +46,7 @@ class Blog extends Component {
         {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
